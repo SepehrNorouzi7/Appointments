7f9ab7e9bc82b19eae5720bd5e077af21da1b4be
SepehrNorouzi7 <snorouzi07@gmail.com>
2024-07-25 14:01:03 +0430

    Add some components
    Some components like search and drop down button have been added.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BsCalendarCheckFill } from "react-icons/bs";
+import { Col, Container, Row } from "react-bootstrap";
+import Search from "./components/Search.js";
+
 function App() {
   return (
     <div className="App">
-      <h1>Appointments</h1>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="text-center fw-light mt-3"><BsCalendarCheckFill /> Appointments</h1>
+          </Col>
+        </Row>
+        <Row className='justify-content-center'>
+          <Col md={3}>
+            <Search />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
